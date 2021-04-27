8f76628f4ddf91b5a9d50253e5c54b9b9bb3fc83
sovernut <orenomail4@gmail.com>
2021-04-27 16:20:27 +0700

    add mealsItem

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const MealItemForm = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+MealItemForm.propTypes = {
+
+}
+
+export default MealItemForm
