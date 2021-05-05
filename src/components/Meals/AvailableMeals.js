bd756d4bbbe3a794e5b383433687535956871a1e
sovernut <orenomail4@gmail.com>
2021-05-06 01:19:18 +0700

    add get meals from firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,45 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./AvailableMeals.module.css";
-import Card from '../UI/Card'
+import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
   {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
     price: 22.99,
   },
   {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
     price: 16.5,
   },
   {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
     price: 12.99,
   },
   {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
     price: 18.99,
   },
 ];
 
 const AvailablesMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price} />)
-  return <section className={classes.meals}>
-    <Card>
+  const [meals, setMeals] = useState(DUMMY_MEALS);
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-http-1cf62-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json"
+      );
+      const loadedMeals = [];
+      const responseData = await response.json();
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+    };
+    fetchMeals();
+  }, []);
 
-      <ul>
-        {mealsList}
-      </ul>
-    </Card>
-  </section>
-}
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
 
-export default AvailablesMeals
+export default AvailablesMeals;
