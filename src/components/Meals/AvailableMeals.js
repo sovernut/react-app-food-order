490689be47769da97b6b91d6cab6b12e1ad69852
sovernut <orenomail4@gmail.com>
2021-05-07 01:00:06 +0700

    handling error

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -32,13 +32,18 @@ const DUMMY_MEALS = [
 
 const AvailablesMeals = () => {
   const [meals, setMeals] = useState(DUMMY_MEALS);
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://react-http-1cf62-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json"
       );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
       const loadedMeals = [];
       const responseData = await response.json();
       for (const key in responseData) {
@@ -50,15 +55,29 @@ const AvailablesMeals = () => {
         });
       }
       setMeals(loadedMeals);
-      setIsLoading(false)
+      setIsLoading(false);
     };
-    fetchMeals();
+
+    fetchMeals().catch((err) => {
+      setIsLoading(false);
+      setHttpError(err.message);
+    });
   }, []);
 
-  if (isLoading){
-    return <section className={classes.MealsLoading}>
-      <p>Loading...</p>
-    </section>
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
   }
 
   const mealsList = meals.map((meal) => (
